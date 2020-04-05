d4b1c6d140f1c809c975a7804773fb9702c6758a
fraviiu <63171227+fraviiu@users.noreply.github.com>
2020-04-05 12:56:48 -0300

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ import os
 telegram_token = os.environ.get('TELEGRAM_BOT_TOKEN')
 
 DB = {
-    'host': os.environ.get('DB_HOST'),
-    'port': int(os.environ.get('DB_PORT')),
-    'user': os.environ.get('DB_USER'),
-    'password': os.environ.get('DB_PASSWORD'),
-    'db': os.environ.get('DB_NAME'),
+    'host': os.environ.get('ec2-34-192-30-15.compute-1.amazonaws.com'),
+    'port': int(os.environ.get('5432')),
+    'user': os.environ.get('bbhgpvqaiehmtx'),
+    'password': os.environ.get('5615c25deae22f2bc3c1d70b0f42c42a678be0baf89e8b1197323ac1699f660f'),
+    'db': os.environ.get('d7ksl4qe7dm863'),
     'charset': 'utf8mb4'
 }
